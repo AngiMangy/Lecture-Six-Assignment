aa7b61bb97eb5654dadcd0eb4592b5b3cea019c3
AngiMangy <kickmango16@gmail.com>
2025-10-02 11:27:13 -0400

    pushing end of class progress

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 // Multiplication Table
+
     function createMultTable(){
         let num = Number(window.prompt("Enter A Number"))
         for(let i = 1; i < 11; i++){
@@ -9,3 +10,29 @@
         }
 
 // Colorful Buttons
+    // Making a for loop that creates 10 buttons with a set class and individual id's
+    let defaultColors = []
+    for(let i = 0; i < 10; i++){
+        let btn = document.createElement("button")
+        btn.id = `btn${i}`
+        btn.className = "btn"
+        btn.innerHTML = "I'm a button"
+        document.getElementById("buttonGroup").appendChild(btn)
+        // giving the buttons their "default" color
+        let baseColor = btn.style.backgroundColor = getRandomColor()
+        defaultColors.push(baseColor)
+    }
+    console.log(`Default Colors ${defaultColors}`)
+    // creating the random color function
+    function getRandomColor() {
+      // Generate a random number between hex code limits
+      let randomColor = Math.floor(Math.random() * 16777215).toString(16);
+      // Pad to make 6-digit code
+      return `#${randomColor.padStart(6, '0')}`;
+    }
+    // color select
+    function colorSelect(x){
+        if(x.value = "red"){
+            document.getElementsByClassName("btn").style.backgroundColor = "Red"
+        }
+    }
